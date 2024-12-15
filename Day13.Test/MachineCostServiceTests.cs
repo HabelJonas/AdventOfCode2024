0a21d3c92897b97adb5777ae4b13848cae4a20c8
Jonas Habel <habeljonas@protonmail.com>
2024-12-15 16:43:55 +0100

    fix tests

--- Day13.Test/MachineCostServiceTests.cs
+++ Day13.Test/MachineCostServiceTests.cs
@@ -15,10 +15,9 @@ public class MachineCostServiceTests
 		Button buttonA = new() { X = 94, Y = 34, Cost = 3 };
 		Button buttonB = new() { X = 22, Y = 67, Cost = 1 };
 		Prize prize = new() { X = 8400, Y = 5400 };
-		MachineCostService mcc = new(buttonA, buttonB, prize);
 
 		// Act
-		int result = mcc.CalculateCost();
+		int result = MachineCostService.CalculateCost(buttonA, buttonB, prize);
 
 		// Assert
 		Assert.Equal(280, result);
@@ -31,10 +30,9 @@ public class MachineCostServiceTests
 		Button buttonA = new() { X = 26, Y = 66, Cost = 3 };
 		Button buttonB = new() { X = 67, Y = 21, Cost = 1 };
 		Prize prize = new() { X = 12748, Y = 12176 };
-		MachineCostService mcc = new(buttonA, buttonB, prize);
 
 		// Act
-		int result = mcc.CalculateCost();
+		int result = MachineCostService.CalculateCost(buttonA, buttonB, prize);
 
 		// Assert
 		Assert.Equal(-1, result);
@@ -47,10 +45,9 @@ public class MachineCostServiceTests
 		Button buttonA = new() { X = 69, Y = 23, Cost = 3 };
 		Button buttonB = new() { X = 27, Y = 71, Cost = 1 };
 		Prize prize = new() { X = 18641, Y = 10279 };
-		MachineCostService mcc = new(buttonA, buttonB, prize);
 
 		// Act
-		int result = mcc.CalculateCost();
+		int result = MachineCostService.CalculateCost(buttonA, buttonB, prize);
 
 		// Assert
 		Assert.Equal(-1, result);
@@ -63,10 +60,9 @@ public class MachineCostServiceTests
 		Button buttonA = new() { X = 17, Y = 86, Cost = 3 };
 		Button buttonB = new() { X = 84, Y = 37, Cost = 1 };
 		Prize prize = new() { X = 7870, Y = 6450 };
-		MachineCostService mcc = new(buttonA, buttonB, prize);
 
 		// Act
-		int result = mcc.CalculateCost();
+		int result = MachineCostService.CalculateCost(buttonA, buttonB, prize);
 
 		// Assert
 		Assert.Equal(200, result);
